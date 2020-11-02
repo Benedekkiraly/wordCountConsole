7c508775314398ab0ff7ba21a507999dc8f2a688
Benedek Kiraly <benedek.kiraly@accent.accenthotels.com>
2020-11-02 17:04:37 +0100

    Add project files.

---
+++ wordCountConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wordCountConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
